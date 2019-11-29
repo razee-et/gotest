ec5ddcbf2974563355fb7dae7bdaa983f034c166
razeekhan <razee.khan@acquia.com>
2019-11-30 01:46:29 +0500

    minor change

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -7,6 +7,10 @@ type client struct {
 	password string
 }
 
+type Raz struct {
+	name string
+}
+
 // Client creates and returns a client for interacting with the message broker.
 func Connect(name string) string {
 	return "razee"
